1572f0487594b0c7a06d745d0baf6ffe3b31ee63
NavaHub <navaa003@gmail.com>
2023-11-03 21:52:03 -0500

    small comment edits in database.js; client install.js handler logic implemented

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// PUT logic that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
 
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
   console.error('Data saved to the database', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// GET logic that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
   
