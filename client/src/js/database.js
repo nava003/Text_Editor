9b4551a967036006b00e36c04ed7965f7f831a3d
NavaHub <navaa003@gmail.com>
2023-11-03 21:15:19 -0500

    client database.js has PUT and GET logic implemented

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  const contactDB = await openDB('jate', 1);
+  const tx = contactDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add( { txt_content: content } );
+
+  const result = await request;
+  console.error('Data saved to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  
+  const contactDB = await openDB('jate', 1);
+  const tx = contactDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  console.error('result.value', result);
+}
 
 initdb();
